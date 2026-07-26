80331f9c2c11ade171e7021feb2640494510bad3
192472100simats-a11y <192472100.simats@saveetha.com>
2026-07-26 12:44:15 +0530

    Add files via upload

---
+++ Lab/Exp 1.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread("TREE.jpg")
+
+if img is None:
+    print("Image not found!")
+else:
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Original Image", img)
+    cv2.imshow("Gray Image", gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ Lab/Exp 2.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread("House.jpeg")
+
+if img is None:
+    print("Image not found!")
+else:
+    blur = cv2.GaussianBlur(img, (9, 9), 0)
+    cv2.imshow("Original Image", img)
+    cv2.imshow("Gaussian Blur Image", blur)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
